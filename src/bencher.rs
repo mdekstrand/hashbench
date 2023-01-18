72ddfdd290060650bd33d249341f59c9b76f5e05
Michael Ekstrand <michaelekstrand@boisestate.edu>
2023-01-17 17:39:43 -0700

    rustfmt

--- src/bencher.rs
+++ src/bencher.rs
@@ -1,47 +1,47 @@
 use std::time::{Duration, Instant};
 
 use crypto_hashes::digest::Digest;
-use rand::RngCore;
 use pcg::Pcg;
+use rand::RngCore;
 
 use log::*;
 
 pub struct HashBencher {
-  data: Vec<u8>,
-  iters: usize,
+    data: Vec<u8>,
+    iters: usize,
 }
 
 impl HashBencher {
-  pub fn new(size: usize, iters: usize) -> HashBencher {
-    let mut data = Vec::with_capacity(size);
-    data.resize(size, 0);
-    let mut rng = Pcg::default();
-    rng.fill_bytes(data.as_mut_slice());
-    HashBencher { data, iters }
-  }
-
-  /// Measure a hash and return duration
-  pub fn measure_hash<H: Digest>(&self, mut hash: H) -> Duration {
-    let start = Instant::now();
-    for _i in 0..self.iters {
-      hash.update(&self.data);
+    pub fn new(size: usize, iters: usize) -> HashBencher {
+        let mut data = Vec::with_capacity(size);
+        data.resize(size, 0);
+        let mut rng = Pcg::default();
+        rng.fill_bytes(data.as_mut_slice());
+        HashBencher { data, iters }
     }
-    let result = hash.finalize();
-    let end = Instant::now();
-    let hash = hex::encode(result);
-    info!("hashed to {}", hash);
-    end.duration_since(start)
-  }
 
-  /// Get the total bytes to hash.
-  pub fn total_bytes(&self) -> usize {
-    self.data.len() * self.iters
-  }
+    /// Measure a hash and return duration
+    pub fn measure_hash<H: Digest>(&self, mut hash: H) -> Duration {
+        let start = Instant::now();
+        for _i in 0..self.iters {
+            hash.update(&self.data);
+        }
+        let result = hash.finalize();
+        let end = Instant::now();
+        let hash = hex::encode(result);
+        info!("hashed to {}", hash);
+        end.duration_since(start)
+    }
 
-  /// Convert a duration to a rate.
-  pub fn duration_to_rate(&self, duration: &Duration) -> f64 {
-    let bytes = self.total_bytes() as f64;
-    let secs = duration.as_secs_f64();
-    bytes / secs
-  }
+    /// Get the total bytes to hash.
+    pub fn total_bytes(&self) -> usize {
+        self.data.len() * self.iters
+    }
+
+    /// Convert a duration to a rate.
+    pub fn duration_to_rate(&self, duration: &Duration) -> f64 {
+        let bytes = self.total_bytes() as f64;
+        let secs = duration.as_secs_f64();
+        bytes / secs
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::io::stdout;
-use std::{path::PathBuf, io::Write};
 use std::fs::File;
+use std::io::stdout;
+use std::{io::Write, path::PathBuf};
 
-use clap::Parser;
 use anyhow::Result;
+use clap::Parser;
 use crypto_hashes::digest::Digest;
-use happylog::clap::LogOpts;
 use friendly::{bytes, duration};
+use happylog::clap::LogOpts;
 use log::*;
 
 mod bencher;
@@ -18,57 +18,92 @@ const MIB_MULT: f64 = 1024.0 * 1024.0;
 
 /// Benchmark Rust cryptographic hash functions.
 #[derive(Debug, Parser)]
-#[command(name="hashbench")]
+#[command(name = "hashbench")]
 struct HashBench {
-  #[command(flatten)]
-  logging: LogOpts,
+    #[command(flatten)]
+    logging: LogOpts,
 
-  /// Specify an output file.
-  #[arg(long="output", short='o')]
-  output: Option<PathBuf>,
+    /// Specify an output file.
+    #[arg(long = "output", short = 'o')]
+    output: Option<PathBuf>,
 
-  /// Specify the size of memory buffer to (repeatedly) hash.
-  #[arg(long="mem-size")]
-  mem_size: Option<usize>,
+    /// Specify the size of memory buffer to (repeatedly) hash.
+    #[arg(long = "mem-size")]
+    mem_size: Option<usize>,
 
-  /// Number of iterations to hash.
-  #[arg(long="iters", short='n')]
-  iters: Option<usize>,
+    /// Number of iterations to hash.
+    #[arg(long = "iters", short = 'n')]
+    iters: Option<usize>,
 }
 
-fn bench_and_print<W: Write, H: Digest, F: Fn() -> H>(out: &mut W, bench: &HashBencher, name: &str, new: F) -> Result<f64> {
-  info!("benchmarking {}", name);
-  let hash = new();
-  let dur = bench.measure_hash(hash);
-  let rate = bench.duration_to_rate(&dur);
-  info!("{}: hashed {} in {} ({})", name, bytes(bench.total_bytes()), duration(dur), bytes(rate).suffix("B/s"));
-  writeln!(out, "{}\t{}", name, rate / MIB_MULT)?;
-  Ok(rate)
+fn bench_and_print<W: Write, H: Digest, F: Fn() -> H>(
+    out: &mut W,
+    bench: &HashBencher,
+    name: &str,
+    new: F,
+) -> Result<f64> {
+    info!("benchmarking {}", name);
+    let hash = new();
+    let dur = bench.measure_hash(hash);
+    let rate = bench.duration_to_rate(&dur);
+    info!(
+        "{}: hashed {} in {} ({})",
+        name,
+        bytes(bench.total_bytes()),
+        duration(dur),
+        bytes(rate).suffix("B/s")
+    );
+    writeln!(out, "{}\t{}", name, rate / MIB_MULT)?;
+    Ok(rate)
 }
 
 fn main() -> Result<()> {
-  let args = HashBench::parse();
-  args.logging.init()?;
+    let args = HashBench::parse();
+    args.logging.init()?;
 
-  let size = args.mem_size.unwrap_or(DEFAULT_MEM_SIZE);
-  let iters = args.iters.unwrap_or(DEFAULT_ITERS);
+    let size = args.mem_size.unwrap_or(DEFAULT_MEM_SIZE);
+    let iters = args.iters.unwrap_or(DEFAULT_ITERS);
 
-  let bench = HashBencher::new(size, iters);
-  info!("initializing benchmark of {} in {} iters", bytes(bench.total_bytes()), iters);
+    let bench = HashBencher::new(size, iters);
+    info!(
+        "initializing benchmark of {} in {} iters",
+        bytes(bench.total_bytes()),
+        iters
+    );
 
-  let mut out: Box<dyn Write> = if let Some(file) = &args.output {
-    Box::new(File::create(file)?)
-  } else {
-    Box::new(stdout())
-  };
+    let mut out: Box<dyn Write> = if let Some(file) = &args.output {
+        Box::new(File::create(file)?)
+    } else {
+        Box::new(stdout())
+    };
 
-  writeln!(&mut out, "hash\tMiB/s")?;
-  bench_and_print(&mut out, &bench, "MD5", crypto_hashes::md5::Md5::new)?;
-  bench_and_print(&mut out, &bench, "SHA-1", crypto_hashes::sha1::Sha1::new)?;
-  bench_and_print(&mut out, &bench, "SHA-256", crypto_hashes::sha2::Sha256::new)?;
-  bench_and_print(&mut out, &bench, "SHA-512", crypto_hashes::sha2::Sha512::new)?;
-  bench_and_print(&mut out, &bench, "SHA3-256", crypto_hashes::sha3::Sha3_256::new)?;
-  bench_and_print(&mut out, &bench, "SHA3-512", crypto_hashes::sha3::Sha3_512::new)?;
+    writeln!(&mut out, "hash\tMiB/s")?;
+    bench_and_print(&mut out, &bench, "MD5", crypto_hashes::md5::Md5::new)?;
+    bench_and_print(&mut out, &bench, "SHA-1", crypto_hashes::sha1::Sha1::new)?;
+    bench_and_print(
+        &mut out,
+        &bench,
+        "SHA-256",
+        crypto_hashes::sha2::Sha256::new,
+    )?;
+    bench_and_print(
+        &mut out,
+        &bench,
+        "SHA-512",
+        crypto_hashes::sha2::Sha512::new,
+    )?;
+    bench_and_print(
+        &mut out,
+        &bench,
+        "SHA3-256",
+        crypto_hashes::sha3::Sha3_256::new,
+    )?;
+    bench_and_print(
+        &mut out,
+        &bench,
+        "SHA3-512",
+        crypto_hashes::sha3::Sha3_512::new,
+    )?;
 
-  Ok(())
+    Ok(())
 }
